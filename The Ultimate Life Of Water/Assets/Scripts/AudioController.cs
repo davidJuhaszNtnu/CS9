32a4523613c2419e63b1fddf51aa772f9d81f50a
davidJuhaszNtnu <115979856+davidJuhaszNtnu@users.noreply.github.com>
2024-03-11 11:55:37 +0100

    11.03.2024

---
+++ The Ultimate Life Of Water/Assets/Scripts/AudioController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+    //-----------------infographics----------------------------
+    //refinery
+    public AudioSource text31, text32, text33;
+    public void text31_bttn(){
+        text31.Play();
+    }
+    public void text32_bttn(){
+        text32.Play();
+    }
+    public void text33_bttn(){
+        text33.Play();
+    }
+    //utility
+    public AudioSource text34;
+    public void text34_bttn(){
+        text34.Play();
+    }
+    //chemical
+    public AudioSource text35, text36, text37;
+    public void text35_bttn(){
+        text35.Play();
+    }
+    public void text36_bttn(){
+        text36.Play();
+    }
+    public void text37_bttn(){
+        text37.Play();
+    }
+    //food
+    public AudioSource text38, text39, text40;
+    public void text38_bttn(){
+        text38.Play();
+    }
+    public void text39_bttn(){
+        text39.Play();
+    }
+    public void text40_bttn(){
+        text40.Play();
+    }
+    //farm
+    public AudioSource text41, text42, text43;
+    public void text41_bttn(){
+        text41.Play();
+    }
+    public void text42_bttn(){
+        text42.Play();
+    }
+    public void text43_bttn(){
+        text43.Play();
+    }
+    //city
+    public AudioSource text44;
+    public void text44_bttn(){
+        text44.Play();
+    }
+    //parameter game
+    public AudioSource text46;
+    public void text46_bttn(){
+        text46.Play();
+    }
+    //warning panel
+    public AudioSource text45;
+    public void text45_bttn(){
+        text45.Play();
+    }
+
+}
